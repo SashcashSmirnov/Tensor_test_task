18f600f565d2a034ffec20cb6203111f5ad7bb02
Sashcash <k079kk@yandex.ru>
2026-04-27 16:29:09 +0500

    fix conftest

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,9 +1,23 @@
+import os
+import sys
+
+import pytest
+
+
 def open_saby_main_page(self):
     self.open("https://saby.ru/")
-
 
 def open_main_page_tensor(self):
     self.open("https://tensor.ru/")
 
+def pytest_addoption(parser):
+    """Добавляем кастомные опции командной строки"""
+    parser.addoption("--headless", action="store_true", default=False, help="Run tests in headless mode")
 
-
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    """Настройка seleniumbase перед запуском тестов"""
+    if config.getoption("--headless"):
+        # Устанавливаем переменные окружения для headless-режима
+        os.environ["HEADLESS"] = "true"
+        os.environ["DISPLAY"] = ":99"
