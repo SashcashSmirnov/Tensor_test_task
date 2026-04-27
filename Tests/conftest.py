0c9f865854550cd41debd4a30cadb738872b15f0
Sashcash <k079kk@yandex.ru>
2026-04-27 16:37:25 +0500

    fix conftest

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import allure
 import pytest
 
 
@@ -10,14 +11,16 @@ def open_saby_main_page(self):
 def open_main_page_tensor(self):
     self.open("https://tensor.ru/")
 
-def pytest_addoption(parser):
-    """Добавляем кастомные опции командной строки"""
-    parser.addoption("--headless", action="store_true", default=False, help="Run tests in headless mode")
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """Добавление скриншота в allure при падении теста"""
+    outcome = yield
+    report = outcome.get_result()
 
-@pytest.hookimpl(tryfirst=True)
-def pytest_configure(config):
-    """Настройка seleniumbase перед запуском тестов"""
-    if config.getoption("--headless"):
-        # Устанавливаем переменные окружения для headless-режима
-        os.environ["HEADLESS"] = "true"
-        os.environ["DISPLAY"] = ":99"
+    if report.when == "call" and report.failed:
+        if hasattr(item.instance, 'driver'):
+            allure.attach(
+                item.instance.driver.get_screenshot_as_png(),
+                name="screenshot",
+                attachment_type=allure.attachment_type.PNG
+            )
